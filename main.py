862a217019fa66bf6963fc94d4331e6ea7f55195
Romain MOREAU <moreau.romain83@gmail.com>
2017-01-26 08:46:02 +0100

    feat(main): get latest tag

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from git import Repo, InvalidGitRepositoryError
 if __name__ == "__main__":
     try:
         repo = Repo(".")
-        print(repo.tags)
+        tag_list = [(tag_ref.commit.authored_date, tag_ref.name) for tag_ref in repo.tags]
+        print(max(tag_list, key=lambda tag: tag[0]))
     except InvalidGitRepositoryError:
         print("This is not a git repository")
         exit()
