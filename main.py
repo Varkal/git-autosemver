a33f835f122d5a1e1ca469b26b23e43d66fc7a5a
Romain MOREAU <moreau.romain83@gmail.com>
2017-02-04 20:02:12 +0100

    feat(main): config

--- main.py
+++ main.py
@@ -2,23 +2,103 @@ from git import Repo, InvalidGitRepositoryError
 import re
 
 
-def is_semver_tag(tag):
-    pattern = re.compile(r"^(\d+)\.(\d+)\.(\d+)$")
-    return pattern.search(tag[1])
+class NoCommitSinceLastTagError(Exception):
+    pass
 
 
-def get_commits_more_recent_than(repo_ref, ref):
-    print(ref)
+def is_semver_tag(tag, config=None):
+    pattern = re.compile(
+        "^" + re.escape(config["prefix"]) +
+        "(\d+)\.(\d+)\.(\d+)" +
+        re.escape(config["suffix"]) + "$"
+    )
+    return pattern.search(tag["name"])
 
 
-if __name__ == "__main__":
+def is_major_commit(commit):
+    pattern = re.compile(r"^.*#major.*$")
+    return pattern.search(commit.message)
+
+
+def is_minor_commit(commit):
+    pattern = re.compile(r"^.*feat.*$")
+    return pattern.search(commit.message)
+
+
+def get_commits_more_recent_than(repo, ref):
+    commits_list = []
+    for commit in repo.iter_commits():
+        if repo.is_ancestor(ref, commit) and commit != ref:
+            commits_list.append(commit)
+    return commits_list
+
+
+def create_tag_dict(tag_ref):
+    return {
+        "timestamp": tag_ref.commit.authored_date,
+        "name": tag_ref.name,
+        "commit": tag_ref.commit
+    }
+
+
+def create_tag_list(tags):
+    return [create_tag_dict(tag_ref) for tag_ref in tags]
+
+
+def get_semver_dict(tag_name):
+    splitted_name = tag_name.split(".")
+    return {
+        "major": int(splitted_name[0]),
+        "minor": int(splitted_name[1]),
+        "patch": int(splitted_name[2])
+    }
+
+
+def get_rev_type(commits):
+    if len(commits) == 0:
+        raise NoCommitSinceLastTagError
+
+    rev_type = "patch"
+    for commit in commits:
+        if rev_type != "major" and is_major_commit(commit):
+            rev_type = "major"
+        if rev_type != "major" and is_minor_commit(commit):
+            rev_type = "minor"
+    return rev_type
+
+
+def calc_new_version(semver_dict, rev_type):
+    semver_dict[rev_type] += 1
+    if rev_type == "major":
+        semver_dict["minor"] = semver_dict["patch"] = 0
+    if rev_type == "minor":
+        semver_dict["patch"] = 0
+    return semver_dict
+
+
+def main():
     try:
         repo = Repo(".")
-        tag_list = [(tag_ref.commit.authored_date, tag_ref.name, tag_ref.commit) for tag_ref in repo.tags]
-        tag_list = [tag for tag in tag_list if is_semver_tag(tag)]
-        last_tag = max(tag_list, key=lambda tag: tag[0])
-        get_commits_more_recent_than(repo, last_tag[2])
+        config = {"prefix": "", "suffix": ""}
+        tag_list = [tag for tag in create_tag_list(repo.tags) if is_semver_tag(tag, config)]
+
+        if len(tag_list):
+            last_tag = max(tag_list, key=lambda tag: tag["timestamp"])
+            commits = get_commits_more_recent_than(repo, last_tag["commit"])
+            rev_type = get_rev_type(commits)
+            semver_dict = calc_new_version(get_semver_dict(last_tag["name"]), rev_type)
+        else:
+            semver_dict = get_semver_dict("1.0.0")
+
+        repo.create_tag("%(major)s.%(minor)s.%(patch)s" % semver_dict)
 
     except InvalidGitRepositoryError:
         print("This is not a git repository")
         exit()
+    except NoCommitSinceLastTagError:
+        print("No commits since last tag")
+        exit()
+
+
+if __name__ == "__main__":
+    main()
